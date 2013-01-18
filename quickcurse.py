0af9ca947cacc66ae11f2d09ffb0d1f2c219bb60
Joshua Barney <qwitwa@gmail.com>
2013-01-18 21:50:19 +0000

    A massive step up.
    Changed appearance by getting rid of lines, and making top bar reverse
    video. Go menu scrolling up and down to work. Removed directories from
    path.
    
    Probably a whole lot more basic stuff, the stuff that came before the
    stuff mentioned above, as a lot has changed since the last commit.

--- quickcurse.py
+++ quickcurse.py
@@ -1,34 +1,52 @@
 import curses
 import curses.textpad
 import os
+import os.path
 import re
 # Initialise master window and turn off character echoing
 screen = curses.initscr()
 curses.noecho()
-ignore_dots = True
 
 #Initial variables
 dims = screen.getmaxyx()
-list_of_files = os.listdir(os.environ["HOME"] + "")
+path = os.environ["HOME"]
+list_of_files = os.listdir(path)
+list_of_items = [] #the selection of list_of_files that are actually shown
+list_pos = 0
 phrase = ""
+ignore_dots = True
+in_topbar = False
+in_leftpane = True
+in_rightpane = False
 
 #Initial screen setup:
-screen.hline(1, 0, "=", dims[1]) 	#Draw a horizontal line under the top bar
-screen.vline(2, 26, "|", dims[0] -2 )
 screen.refresh()
 
-topbar = curses.newwin(1, dims[1], 0, 0)#Create a one line window at the top
-topbox = curses.textpad.Textbox(topbar)	#Using the just created window, create a textbox
+topbar = curses.newwin(1, dims[1], 0, 0)
+topbox = curses.textpad.Textbox(topbar)
+topbar.bkgd(" ", curses.A_REVERSE)
+topbar.refresh()
 
-leftpane = curses.newwin(dims[0] - 2, 25, 2, 0) #Create left pane window under what has gone before
+leftpane = curses.newwin(dims[0] - 2, 25, 2, 0)
+leftpane.keypad(1)
 leftpane.refresh()
 
+rightpane = curses.newwin(dims[0] - 2, dims[1] - 26, 2, 26)
+rightpane.addstr(0, 0, "Test")
+rightpane.refresh
+
 def update_leftpane():
+	global list_of_items
 	leftpane.clear()
 	pos = 0
+	list_of_items = []
 	for name in sorted(list_of_files):
-		if re.match(".*" + phrase + ".*", name) and dotfile_test(name) and pos < dims[0] - 2 :
-			leftpane.addnstr(pos, 0, name, 25 )
+		if re.match(".*" + phrase + ".*", name) and dotfile_test(name) and os.path.isfile(path + "/" + name)  and pos < dims[0] - 2 :
+			list_of_items.append(name)
+			if pos == list_pos:
+				leftpane.addnstr(pos, 0, name, 25, curses.A_BOLD) 
+			else:
+				leftpane.addnstr(pos, 0, name, 25)
 			pos += 1
 	leftpane.refresh()
 
@@ -48,14 +66,33 @@ def get_top_bar():
 		exit()
 	topbar.refresh()
 
-#This is temporarily the main event loop. 
+#This is the main event loop. 
 while True:
-	update_leftpane()
-	get_top_bar()
-	screen.refresh()
+	if in_topbar == True:
+		get_top_bar()
+		list_pos = 0
+		in_topbar = False
+		in_leftpane = True
 
-#This is where things get complicated. We need to implement a validator function on that textbox input, so that:
-#	+ Pressing enter works correctly (submits the text, and then deletes the textbox contents - at the moment the former is true but not the latter)
-#	+ Backspaces work correctly 
+	update_leftpane()
+
+	if in_rightpane == True:
+		pass
+	if in_leftpane == True:
+		curses.napms(40)
+		key = leftpane.getch()
+		if key == curses.KEY_DOWN:
+			if list_pos < len(list_of_items) - 1:
+				list_pos += 1
+		elif key == curses.KEY_UP:
+			if list_pos > 0:
+				list_pos -= 1
+		elif key == ord("q"):
+			curses.endwin()
+			exit()
+		elif key == 27: #escape 
+			in_leftpane = False
+			in_topbar = True
+	screen.refresh()
 
 
