6d263011a57cc6c18e265b94426d77ea219643cb
qwitwa <qwitwa@gmail.com>
2013-02-28 21:12:19 +0000

    Commited old work
    From the look of the code, the thing most striking is that code to
    process commands has been added in. However, it's slapdash and will
    probably be chucked out at some later date in favour of a better way of
    doing things.

--- quickcurse.py
+++ quickcurse.py
@@ -1,3 +1,4 @@
+# Something strange is happening when scrolling, only allowing scroll through 2 list items. When scrolling up, the description doesn't change.   
 import curses
 import curses.textpad
 import os
@@ -5,31 +6,32 @@ import os.path
 import re
 import subprocess
 
-# Initialise master window and turn off character echoing
+# Initialise master window, turn off character echoing, and disable the cursor.
 screen = curses.initscr()
 curses.noecho()
 curses.curs_set(0)
 
 #Initial variables
-dims = screen.getmaxyx()
-path = os.environ["HOME"] + "/Dropbox/plain text"
-list_of_files = os.listdir(path)
+dims = screen.getmaxyx() #tuple containing the maximum screen dimensions.
+path = os.environ["HOME"] + "/Dropbox/plain text" #set the path to get notes from.
+list_of_files = os.listdir(path) #grab the names of all the files in the specified path
 list_of_items = [] #the selection of list_of_files that are actually shown
-files = {}
+files = {} #contains the content of every file ever shown
+files["default"] = "Well, here's some default content"
 list_pos = 0
-ignore_dots = True
+ignore_dots = True #Toggle hidden files
 focus = "left" #left or right
 
 #Initial screen setup:
 screen.refresh()
 
-topbar = curses.newwin(1, dims[1], 0, 0)
+topbar = curses.newwin(1, dims[1], 0, 0) #the bar to type in
 topbar.refresh()
 
-rightpane = curses.newwin(dims[0] - 2, dims[1] - 28, 2, 28)
+rightpane = curses.newwin(dims[0] - 2, dims[1] - 28, 2, 28) #the pane for showing note content
 rightpane.refresh()
 
-leftpane = curses.newwin(dims[0] - 2, 25, 2, 0)
+leftpane = curses.newwin(dims[0] - 2, 25, 2, 0) #the pane for showing note names
 leftpane.keypad(1)
 leftpane.refresh()
 
@@ -42,7 +44,6 @@ class stringbuffer:
 		length = len(self.stringdata)
 		self.stringdata = self.stringdata[:length - 1]
 
-phrase = stringbuffer()
 
 def update_leftpane(create = False):
 	global list_of_items, files
@@ -50,7 +51,7 @@ def update_leftpane(create = False):
 	pos = 0
 	list_of_items = []
 	for name in sorted(list_of_files):
-		if re.match("(?i).*" + phrase.stringdata + ".*" + "\.txt$", name) and notdotfile(name) and os.path.isfile(path + "/" + name)  and pos < dims[0] - 2 :
+		if re.match("(?i).*" + phrase.stringdata + ".*" + "\.txt$", name) and notdotfile(name) and os.path.isfile(path + "/" + name) and pos < dims[0] - 2 :
 			list_of_items.append(name)
 			if not name in files:
 				fd_being_slurped = open(path + "/" + name)
@@ -66,12 +67,18 @@ def update_leftpane(create = False):
 
 	leftpane.refresh()
 
+
+
 def update_rightpane():
 	rightpane.clear()
-	if list_pos in list_of_items:
+	if list_pos < len(list_of_items):
 		name = list_of_items[list_pos]
-	else : name = ""
+	else: name = "default"
 	if name in files:
+		rightpane.addstr(0, 0, files[name])
+	else: 
+		fd_being_slurped = open(path + "/" + name)
+		files[name] = fd_being_slurped.read()
 		rightpane.addstr(0, 0, files[name])
 	rightpane.refresh()
 
@@ -88,21 +95,38 @@ def notdotfile(name):
 	else:
 		return True
 
+def get_command():
+	topbar.clear()
+	topbar.addstr(0, 0, "command: ")
+	topbar.refresh()
+	key = leftpane.getch()
+	if key == ord("q"):
+		if show_command("quit"):
+			curses.endwin()
+			exit()
+	topbar.clear()
+	topbar.addstr(0, 0, phrase.stringdata)
+	topbar.refresh()
 
+def show_command(command):
+	topbar.addstr(0, 9, command[:1], curses.A_BOLD)
+	topbar.addstr(0, 10, command[1:])
+	topbar.refresh()
+	key = topbar.getch()
+	if key == 10:
+		return True
+	else:
+		return False
+
+phrase = stringbuffer()
 update_topbar()
 update_leftpane()
 update_rightpane()
 
-
-def get_command():
-	key = leftpane.getch()
-	if key == ord("q"):
-		curses.endwin()
-		exit()
-
 #This is the main event loop.
 while True:
 	update_leftpane(True)
+	update_rightpane()
 	list_pos = 0
 	if focus == "right":
 		pass
@@ -110,7 +134,7 @@ while True:
 		curses.napms(40)
 		key = leftpane.getch()
 		if key == curses.KEY_DOWN:
-			if list_pos < len(list_of_items) -1:
+			if list_pos < len(list_of_items) - 2 :
 				list_pos += 1
 				update_rightpane()
 		elif key == curses.KEY_UP:
@@ -122,9 +146,11 @@ while True:
 		elif re.match("[A-Za-z0-9]", chr(key)):
 			phrase.append(chr(key))
 			update_topbar()
+			update_leftpane()
 			update_rightpane()
 		elif key == 127: #backspace
 			phrase.backspace()
 			update_topbar()
+			update_leftpane()
 			update_rightpane()
 
